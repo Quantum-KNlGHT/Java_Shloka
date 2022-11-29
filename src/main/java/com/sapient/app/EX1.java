dcb992aff6f3ddb6b3cef1625b645baa906e2aae
shloka_chowdhury <shloka.chowdhury@publicissapient.com>
2022-11-29 22:38:18 +0530

    Added

---
+++ src/main/java/com/sapient/app/EX1.java
@@ -0,0 +1,13 @@
+package com.sapient.app;
+
+public class EX1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstname="Shloka";
+		String lastname="Chowdhury";
+		System.out.println("Hello "+firstname+" "+lastname);
+
+	}
+
+}
